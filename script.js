24dad3cd1be0a12e01bf3dbad07e1a9273968d95
praise <prayzstanley@gmail.com>
2025-11-10 18:15:02 +0100

    sixth commit

---
+++ script.js
@@ -0,0 +1,6 @@
+if(username=== "exampleInputEmail1" && password==="exampleInputPassword1"){
+    alert("Login Successful!");
+    window.location.href = 'dashboard.html';
+}else{
+    alert('Invalid username or password');
+}
