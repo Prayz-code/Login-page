e10eeee14011b53691c65aceb549057f31638446
praise <prayzstanley@gmail.com>
2025-11-12 11:53:07 +0100

    eighth commit

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+debugger
+let username = "makecodelit@gmail.com";
+let password = "ilovecoding123";
+
 if(username === "makecodelit@gmail.com" && password==="ilovecoding123"){
-    alert("Login Successful!");
-     window.location.assign("dashboard.html");
+    alert("login successful!");
+    // const login = document.getElementById("login");
+
+    // login.addEventListener("click", () => alert("Login Successful! Welcome!"));
+    // window.location = ("dashboard.html");
 }else{
-    alert('Invalid username or password');
+    alert("invalid username");
+    // debugger
+    // const invaliduser = document.getElementById("login");
+    // invaliduser.addEventListener("click", () => textContent = "Invalid username or password");
 };
 
-function newPage(){
-    window.location.assign("dashboard.html")
-};
